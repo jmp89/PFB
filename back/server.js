d73ef303afb69aca752e689f7eb9cb949b86f6bf
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-06-11 20:05:28 +0100

    Added 404 and login endpoint

--- back/server.js
+++ back/server.js
@@ -1,15 +1,24 @@
-"use strict"
+"use strict";
 
-import "dotenv/config.js"
-import express from "express"
+import "dotenv/config.js";
+import express from "express";
 
-const app = express()
+const app = express();
 
-const {PORT} = process.env
+const { PORT } = process.env;
 
 app.get("/", (req, res) => {
-    res.send("¡Primer endpoint del PFB!")
-})
+  res.send("¡Primer endpoint del PFB!");
+});
 
+app.get("/login", (req, res) => {
+  res.status(201).send("Login");
+});
 
-app.listen(PORT, ()=> console.log("Server running on http://localhost:" + PORT))
+app.use((req, res) => {
+  res.status(404).send("Ruta no encontrada");
+});
+
+app.listen(PORT, () =>
+  console.log("Server running on http://localhost:" + PORT)
+);
